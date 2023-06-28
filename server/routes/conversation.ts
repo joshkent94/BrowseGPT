b24ecdc4209c8c4fe9221ed43d71b126803faee8
Josh Kent <josh.kent94@yahoo.co.uk>
2023-06-28 23:34:57 +0100

    Use internal API key

--- server/routes/conversation.ts
+++ server/routes/conversation.ts
@@ -5,15 +5,13 @@ import {
     OpenAIApi,
     ChatCompletionRequestMessageRoleEnum,
 } from 'openai'
-import { decryptString } from '@utils/decryptString'
 import { getCityAndCountry } from '@utils/getCityAndCountry'
+require('dotenv').config()
 
 const conversation = procedure
     .input(
         z.object({
             firstName: z.string(),
-            apiKey: z.string(),
-            salt: z.string(),
             conversation: z.object({
                 id: z.string(),
                 isOpen: z.boolean(),
@@ -33,8 +31,7 @@ const conversation = procedure
     )
     .query(async (opts) => {
         const name = opts.input.firstName
-        const apiKey = opts.input.apiKey
-        const salt = opts.input.salt
+        const apiKey = process.env.OPENAI_API_KEY
         let location: string | undefined
         const [latitude, longitude] = opts.input.conversation.userLocation
         if (latitude && longitude) {
@@ -60,27 +57,26 @@ const conversation = procedure
             month: 'long',
             day: 'numeric',
         })
-        const decryptedKey = decryptString(apiKey, salt)
         const configuration = new Configuration({
-            apiKey: decryptedKey,
+            apiKey
         })
         const openAI = new OpenAIApi(configuration)
-        // const response = await openAI.createChatCompletion({
-        //     model: 'gpt-3.5-turbo',
-        //     messages: [
-        //         {
-        //             role: 'system',
-        //             content: `The date is ${date}. You are an AI assistant that aids users as they browse the web, called BrowseGPT. You are helpful, creative and clever. You will be sent messages in the form '{user_current_url}: {user_message}' where user_current_url is the URL the user was on when sending the message, and user_message is the message the user is sending. Use this information when providing your answer. The user can click links you send them, so favour sending links in your messages. Also, favour replying with short, concise messages so the user can get what they want quickly, and never reply with code or JSON. You are helping ${name} who's currently in ${location}. Introduce yourself in one extremely short sentence, and make sure to speak to them like a close friend.`,
-        //         },
-        //         ...messagesToSend,
-        //     ],
-        // })
-        // return response.data.choices[0].message
-        // ! Comment this out and uncomment the above to use OpenAI
-        return {
-            role: 'assistant',
-            content: 'Hi Josh!',
-        }
+        const response = await openAI.createChatCompletion({
+            model: 'gpt-3.5-turbo',
+            messages: [
+                {
+                    role: 'system',
+                    content: `The date is ${date}. You are an AI assistant that aids users as they browse the web, called BrowseGPT. You are helpful, creative and clever. You will be sent messages in the form '{user_current_url}: {user_message}' where user_current_url is the URL the user was on when sending the message, and user_message is the message the user is sending. Use this information when providing your answer. The user can click links you send them, so favour sending links in your messages. Also, favour replying with short, concise messages so the user can get what they want quickly, and never reply with code or JSON. You are helping ${name} who's currently in ${location}. Introduce yourself in one extremely short sentence, and make sure to speak to them like a close friend.`,
+                },
+                ...messagesToSend,
+            ],
+        })
+        return response.data.choices[0].message
+        // // ! Comment this out and uncomment the above to use OpenAI
+        // return {
+        //     role: 'assistant',
+        //     content: 'Hi Josh!',
+        // }
     })
 
 export { conversation }
--- server/utils/decryptString.ts
+++
@@ -1,5 +0,0 @@
-import CryptoJS from 'crypto-js'
-
-export const decryptString = (encryptedStr: string, salt: string): string => {
-    return CryptoJS.AES.decrypt(encryptedStr, salt).toString(CryptoJS.enc.Utf8)
-}
