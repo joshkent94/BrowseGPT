a322c003a1eb794a7530ded3621c6bafb8b2231a
Josh Kent <josh.kent94@yahoo.co.uk>
2023-07-22 11:23:02 +0100

    Use SSL in production

--- server/utils/trpc.ts
+++ server/utils/trpc.ts
@@ -1,19 +1,17 @@
 import { PrismaClient } from '@prisma/client'
 import { initTRPC, inferAsyncReturnType } from '@trpc/server'
 import { CreateExpressContextOptions } from '@trpc/server/adapters/express'
+import { connectionString } from '@utils/middleware/sessionConfig'
 
 export const createContext = async ({
     req,
     res,
 }: CreateExpressContextOptions) => {
     const session = req.session
-    const url =
-        process.env.NODE_ENV ===
-        'production' ? `${process.env.DATABASE_URL}?schema=public&sslmode=require` : process.env.DATABASE_URL
     const prisma = new PrismaClient({
         datasources: {
             db: {
-                url,
+                url: connectionString,
             },
         },
     })
